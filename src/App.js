360ba5a1e0332694f8a6da0404804de6b72e2424
Axel Langerman <solomonsilversmith@gmail.com>
2022-10-25 20:28:36 -0300

    Preparando entrega final

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ import { ItemListContainer } from './components/ItemListContainer/ItemListContai
 import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer';
 import { Cart } from './components/Cart/Cart';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {CartProvider} from './context/CartContext';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <NavBar/>
-        <Routes>
-          <Route path='/' element={<ItemListContainer/>}/>
-          <Route path='/categoria/:categoriaId' element={<ItemListContainer/>}/>
-          <Route path='/cart' element={<Cart/>}/>
-          <Route path='/detalle/:detalleId' element={<ItemDetailContainer/>}/>
-        </Routes>
+        <CartProvider>
+          <NavBar/>
+          <Routes>
+            <Route path='/' element={<ItemListContainer/>}/>
+            <Route path='/categoria/:categoriaId' element={<ItemListContainer/>}/>
+            <Route path='/cart' element={<Cart/>}/>
+            <Route path='/detalle/:detalleId' element={<ItemDetailContainer/>}/>
+          </Routes>
+        </CartProvider>
       </BrowserRouter>
     </div>
   );
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -2,13 +2,17 @@ import './ItemDetail.css';
 import React, {useState} from 'react';
 import { ItemCount } from '../ItemCount/ItemCount';
 import { Link } from 'react-router-dom';
+import { useCartContext } from '../../context/CartContext';
 
 export const ItemDetail = ({data}) => {
 
     const [goToCart, setGoToCart] = useState(false);
 
+    const {addProduct} = useCartContext();
+
     const onAdd = (quantity) => {
         setGoToCart(true);
+        addProduct(data, quantity);
     }
 
     return(
