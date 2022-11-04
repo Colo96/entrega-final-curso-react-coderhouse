fb2c4f67b22c11554d73715527957de9f50570df
Axel Langerman <solomonsilversmith@gmail.com>
2022-11-04 16:48:42 -0300

    Preparando entrega final

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,5 +1,5 @@
 import './Cart.css';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useCartContext } from '../../context/CartContext';
 import { Link } from 'react-router-dom';
 import { ItemCart } from '../ItemCart/ItemCart';
@@ -19,11 +19,20 @@ export const Cart = () => {
         total: totalPrice()
     });
 
-    const handleInputChange  = (event) => {
+    const handleInputChange = (event) => {
         setOrder({
             ...order,
-            [event.target.name] : event.target.value
+            [event.target.name]: event.target.value
         });
+    }
+
+    const validateCamp = (e) => {
+          if(e.target.name === 'nombre'){
+
+          }
+          if(e.target.value === ''){
+             console.log("El campo " + e.target.name + " no puede estar vacio. Ten en cuenta que no se terminara la compra si este campo sigue vacio.");
+          }
     }
 
     const handleClick = () => {
@@ -35,52 +44,56 @@ export const Cart = () => {
     if (cart.length === 0) {
         return (
             <div className="container-cart">
-                <p>
-                    No hay elementos en el carrito
-                </p>
-                <Link to='/'>
-                    Hacer compras
-                </Link>
+                <div className='cart-empty'>
+                    <p>
+                        No hay elementos en el carrito
+                    </p>
+                    <Link to='/'>
+                        Hacer compras
+                    </Link>
+                </div>
             </div>
         );
     }
 
     return (
         <div className="container-cart">
-            {
-                cart.map(product => <ItemCart key={product.id} product={product} />)
-            }
-            <p>
-                Precio Total: ${totalPrice()}
-            </p>
-            <h2>
-                Por favor complete el formulario para finalizar la compra
-            </h2>
-            <form>
-                <label>
-                    <span>Nombre:</span><br />
-                    <input type="text" name="nombre" placeholder='Nombre' onChange={handleInputChange} />
-                </label>
+            <div className='cart-full'>
+                {
+                    cart.map(product => <ItemCart key={product.id} product={product} />)
+                }
+                <p>
+                    Precio Total: ${totalPrice()}
+                </p>
+                <h2>
+                    Por favor complete el formulario para finalizar la compra
+                </h2>
+                <form>
+                    <label>
+                        <span>Nombre:</span><br />
+                        <input type="text" name="nombre" placeholder='Nombre' onBlur={validateCamp} onChange={handleInputChange} />
+                    </label>
+                    <br />
+                    <label>
+                        <span>Apellido:</span><br />
+                        <input type="text" name="apellido" placeholder='Apellido' onBlur={validateCamp} onChange={handleInputChange} />
+                    </label>
+                    <br />
+                    <label>
+                        <span>Teléfono:</span><br />
+                        <input type="tel" name="telefono" placeholder='12-3454-6789' pattern="[0-9]{2}-[0-9]{4}-[0-9]{4}" onBlur={validateCamp} onChange={handleInputChange} />
+                    </label>
+                    <br />
+                    <label>
+                        <span>Email:</span><br />
+                        <input type="email" name="email" placeholder='algo@coldmail.com' onBlur={validateCamp} onChange={handleInputChange} />
+                    </label>
+                </form>
                 <br />
-                <label>
-                    <span>Apellido:</span><br />
-                    <input type="text" name="apellido" placeholder='Apellido' onChange={handleInputChange} />
-                </label>
-                <br />
-                <label>
-                    <span>Teléfono:</span><br />
-                    <input type="number" name="telefono" placeholder='Teléfono' onChange={handleInputChange} />
-                </label>
-                <br />
-                <label>
-                    <span>Email:</span><br />
-                    <input type="email" name="email" placeholder='Email'  onChange={handleInputChange} />
-                </label>
-            </form>
-            <br />
-            <button onClick={handleClick}>
-                Emitir compra
-            </button>
+                <button onClick={handleClick}>
+                    Emitir compra
+                </button>
+            </div>
         </div>
     );
 }
